4287e20e69ace406226f671547c8aac90038a123
riooastfu <rio.alaska123@gmail.com>
2023-03-21 10:32:05 +0700

    Fixing DataKaryawanController

--- controllers/DataKaryawanController.js
+++ controllers/DataKaryawanController.js
@@ -1,19 +1,33 @@
-import DataUser from "../models/DataKaryawanModel.js";
-import Departemen from "../models/DepartemenModel.js";
+import { QueryTypes } from "sequelize";
 
+import DataUser from "../models/DataKaryawanModel.js";
+
+export const getDataKaryawan = async (req, res) => {
+    try {
+        const dataKaryawan = await DataUser.sequelize.query("SELECT * FROM pers_datakaryawan", {
+            type: QueryTypes.SELECT
+        });
+
+        let response = {
+            record: dataKaryawan
+        }
+        res.json(response);
+
+    } catch (error) {
+        res.json(error);
+    }
+}
 
 export const getDataKaryawanByNik = async (req, res) => {
     try {
-        const dataKaryawan = await DataUser.findOne({
-            include: [
-                { model: Departemen, attributes: ['kode', 'desc'] }
-            ],
-            where: {
-                nik: req.params.nik
-            },
-            attributes: ['nama_karyawan', 'nik', 'status', 'jabatan', 'golongan', 'departemen', 'perusahaan', 'lokasi']
-        });
-        res.json(dataKaryawan)
+        const dataKaryawan = await DataUser.sequelize.query(
+            "SELECT golongan, status, nama_karyawan, pers_departemen.desc as Departemen, pers_jabatan.desc as Jabatan, pers_lokasi.lokasi as Lokasi, pers_pt.nama_pt as PT FROM pers_datakaryawan LEFT JOIN pers_departemen ON pers_datakaryawan.departemen = pers_departemen.kode LEFT JOIN pers_jabatan ON pers_datakaryawan.jabatan = pers_jabatan.kode LEFT JOIN pers_lokasi ON pers_datakaryawan.lokasi = pers_lokasi.kode LEFT JOIN pers_pt ON pers_datakaryawan.perusahaan = pers_pt.kode WHERE nik = :nonik",
+            {
+                replacements: { nonik: req.params.nik },
+                type: QueryTypes.SELECT
+            });
+        res.json(dataKaryawan);
+
     } catch (error) {
         res.json(error);
     }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 
 import { getUsers, Login } from "../controllers/Users.js";
-import { getDataKaryawanByNik } from "../controllers/DataKaryawanController.js";
+// import { getDataKaryawanByNik } from "../controllers/DataKaryawanController.js";
+import { getDataKaryawan, getDataKaryawanByNik } from "../controllers/DataKaryawanController.js";
 import { getData } from "../controllers/DepartemenController.js";
 
 const router = express.Router();
@@ -11,7 +12,9 @@ router.get('/users', getUsers);
 router.post('/login', Login);
 
 //DataKaryawan
-router.get('/dtkaryawan/:nik', getDataKaryawanByNik);
+// router.get('/dtkaryawan/:nik', getDataKaryawanByNik);
+router.get('/dataKaryawan', getDataKaryawan);
+router.get('/dataKaryawan/:nik', getDataKaryawanByNik);
 
 //Departemen
 router.get('/departemen/:kode', getData);
