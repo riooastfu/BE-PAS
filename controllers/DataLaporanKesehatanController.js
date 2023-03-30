b62fa17b1c8dec697ffb914760abf5e88a34cda0
riooastfu <rio.alaska123@gmail.com>
2023-03-30 13:27:38 +0700

    update 2023 03 30

--- controllers/DataLaporanKesehatanController.js
+++ controllers/DataLaporanKesehatanController.js
@@ -5,7 +5,7 @@ import DtLapKes from "../models/DataLaporanKesehatanModel.js";
 export const getDtLapKesByNik = async (req, res) => {
     try {
         const dtLapKes = await DtLapKes.sequelize.query(
-            "SELECT * FROM pers_datalaporan_kesehatan WHERE nik = :nonik ORDER BY tanggal DESC LIMIT 0,8",
+            "SELECT * FROM pers_datalaporan_kesehatan WHERE nik = :nonik ORDER BY tanggal DESC LIMIT 0,10",
             {
                 replacements: { nonik: req.params.nik },
                 type: QueryTypes.SELECT
@@ -35,25 +35,25 @@ export const getDtLapKesById = async (req, res) => {
 export const deleteDtLapKesByTgl = async (req, res) => {
     try {
         const dtLapKes = await DtLapKes.sequelize.query("DELETE FROM pers_datalaporan_kesehatan WHERE id_laporan = :id",
-        {
-            replacements: {id: req.params.id_laporan},
-            type: QueryTypes.DELETE
-        });
+            {
+                replacements: { id: req.params.id_laporan },
+                type: QueryTypes.DELETE
+            });
         res.json("Berhasil Delete");
     } catch (error) {
         res.json(error);
     }
 }
 
-export const createLapKes = async (req,res) => {
+export const createLapKes = async (req, res) => {
     try {
-        const {id_laporan, nik, nik_kantor, tanggal, jam_masuk, jam_pulang, status_kerja, kesehatan_tanggal, kesehatan_nama, kesehatan_dept, kesehatan_jabatan, kesehatan_pt, kesehatan_suhu, kesehatan_keluarga, kesehatan_kontak, kesehatan_resiko, kesehatan_pagi, kesehatan_malam, kesehatan_berobat} = req.body
+        const { id_laporan, nik, nik_kantor, tanggal, jam_masuk, jam_pulang, status_kerja, kesehatan_tanggal, kesehatan_nama, kesehatan_dept, kesehatan_jabatan, kesehatan_pt, kesehatan_suhu, kesehatan_keluarga, kesehatan_kontak, kesehatan_resiko, kesehatan_pagi, kesehatan_malam, kesehatan_berobat } = req.body
         await DtLapKes.create(req.body, {
-            where:{
+            where: {
                 nik: req.params.nik
             }
         });
-        res.status(201).json({msg: "Data Ditambah!"})
+        res.status(201).json({ msg: "Data Ditambah!" })
     } catch (error) {
         res.json("Error: ", error)
     }
